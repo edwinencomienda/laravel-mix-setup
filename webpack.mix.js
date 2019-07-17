d21304dd1ea88f196cacbb801d29880b033f843a
Edwin Encomienda <encomienda000@gmail.com>
2019-07-17 16:17:55 +0800

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,37 @@
+let mix = require('laravel-mix');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+require('mix-html-builder');
+
+mix
+    .webpackConfig({
+        plugins: [
+            new CleanWebpackPlugin({
+                cleanOnceBeforeBuildPatterns: ['dist'],
+                verbose: true,
+            })
+        ],
+    })
+    .js('src/js/app.js', 'dist/js/')
+    .sass('src/sass/app.scss', 'dist/css/')
+    .copyDirectory('src/img', 'dist/img');
+
+
+mix.browserSync({
+    proxy: false,
+    notify: false,
+    server: {
+        baseDir: './src'
+    },
+    files: ['./src/**']
+});
+
+mix.html({
+    htmlRoot: './src/index.html', // Your html root file
+    output: 'dist', // The html output folder
+    partialRoot: './src/partials',    // default partial path
+    minify: {
+        removeComments: true
+    }
+});
+
+mix.disableSuccessNotifications();
